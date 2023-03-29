3fd5a5f339ea6e910c60598bc209474204c50e39
brucvei <bruna.caetano@acad.ufsm.br>
2023-03-29 11:09:08 -0300

    error 404 handeler

--- demo/src/main/java/com/bruna/demo/services/CategoriaService.java
+++ demo/src/main/java/com/bruna/demo/services/CategoriaService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 
 import com.bruna.demo.domain.Categoria;
 import com.bruna.demo.repositories.CategoriaRepository;
+import com.bruna.demo.services.exceptions.ObjectNotFoundException;
+
 
 @Service
 public class CategoriaService {
@@ -16,7 +18,12 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(
+				(() -> new ObjectNotFoundException(
+						"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()
+				)
+			)
+		);
 	}
 
 }
